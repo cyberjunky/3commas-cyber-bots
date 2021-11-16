113d1835eeb213ccd2843a4bdd5cda881ea9e28b
Ben Hählen <git@benweb.eu>
2021-11-16 10:06:26 +0100

    Updated SO calculation, because I made a boo-boo and git is not liking it

--- compound.py
+++ compound.py
@@ -350,12 +350,7 @@ def compound_bot(thebot):
 
             # Calculate compound values
             boprofitsplit = ((profitsum * bopercentage) / 100) / max_active_deals
-            soprofitsplit = (
-                ((profitsum * sopercentage) / 100)
-                / max_active_deals
-                / max_safety_orders
-                * ratiofunds
-            )
+            soprofitsplit = boprofitsplit * (safety_order_size / base_order_size)
 
             logger.info("BO compound value: %s" % boprofitsplit)
             logger.info("SO compound value: %s" % soprofitsplit)
