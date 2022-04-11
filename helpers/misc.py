26a8e91c630617ab2d1b7092586605f85c67e9df
Amargedon <amargedon@gmail.com>
2022-04-11 15:34:31 +0200

    Have the option to notify about update. Disable from script if desired

--- helpers/misc.py
+++ helpers/misc.py
@@ -16,7 +16,7 @@ def wait_time_interval(logger, notification, time_interval, notify=True):
         nexttime = localtime + int(time_interval)
         timeresult = time.strftime("%H:%M:%S", time.localtime(nexttime))
         logger.info(
-            "Next update in %s Seconds at %s" % (time_interval, timeresult)
+            "Next update in %s Seconds at %s" % (time_interval, timeresult), notify
         )
         notification.send_notification()
         time.sleep(time_interval)
