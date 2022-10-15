54b74ffc2388f140b7fc0c2977f2ac7d099817ba
Amargedon <amargedon@gmail.com>
2022-10-15 17:42:07 +0200

    Moved from Playwright to cloudscraper

--- helpers/browser.py
+++ helpers/browser.py
@@ -1,6 +1,6 @@
 """Cyberjunky's 3Commas bot helpers."""
+import cloudscraper
 import json
-from playwright.sync_api import sync_playwright
 
 def get_shared_bot_data(logger, bot_id, bot_secret):
     """Get the shared bot data from the 3C website"""
@@ -9,22 +9,25 @@ def get_shared_bot_data(logger, bot_id, bot_secret):
 
     data = {}
     try:
-        with sync_playwright() as p:
-            # Webkit is fastest to start and hardest to detect
-            browser = p.webkit.launch(headless=True)
+        statuscode = 0
+        scrapecount = 0
+        while (scrapecount < 3) and (statuscode != 200):
+            scraper = cloudscraper.create_scraper(interpreter = "nodejs", delay = scrapecount * 6, debug = False)
 
-            page = browser.new_page()
-            page.goto(url)
+            page = scraper.get(url)
+            statuscode = page.status_code
 
-            # Use evaluate instead of `content` not to import bs4 or lxml
-            html = page.evaluate('document.querySelector("pre").innerText')
+            logger.debug(
+                f"Status {statuscode} for bot {bot_id}"
+            )
 
-        try:
-            data = json.loads(html)
+            if statuscode == 200:
+                data = json.loads(page.text)
+                logger.info("Fetched %s 3C shared bot data OK" % (bot_id))
 
-            logger.info("Fetched %s 3C shared bot data OK" % (bot_id))
-        except:
-            # Still might fail sometimes
+            scrapecount += 1
+        
+        if statuscode != 200:
             data = None
             logger.error("Failed to fetch %s 3C shared bot data" % (bot_id))
 
